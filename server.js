c09bd1c8e0164891620b8abb5d25f98841361dac
Farhan12Anwar <anwarfarhan339@gmail.com>
2024-11-03 12:10:24 +0530

    Updated

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
 // server.js
 
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const mongoose = require('mongoose');
-const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
-const Message = require('./public/models/Message'); // Import the Message model
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const mongoose = require("mongoose");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+const Message = require("./public/models/Message"); // Import the Message model
+require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://anwarfarhan339:cannonx100@cluster0.e121f2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // Check if MongoDB connection is successful
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // const botName = 'Auction Bot';
 
 // Run when client connects
-io.on('connection', (socket) => {
-  socket.on('joinRoom', async ({ username, room }) => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", async ({ username, room }) => {
     const user = userJoin(socket.id, username, room);
 
     socket.join(user.room);
@@ -45,26 +51,17 @@ io.on('connection', (socket) => {
 
     // Send previous messages to the user
     messages.forEach((message) => {
-      socket.emit('message', message);
+      socket.emit("message", message);
     });
 
-    // Welcome current user
-    // socket.emit('message', formatMessage(botName, 'Welcome to the Auction Chat'));
-
-    // Broadcast when a user connects
-    // socket.broadcast
-    //   .to(user.room)
-    //   .emit('message', formatMessage(botName, `${user.username} has joined the chat`));
-
-    // Send users and room info
-    io.to(user.room).emit('roomUsers', {
+    io.to(user.room).emit("roomUsers", {
       room: user.room,
       users: getRoomUsers(user.room),
     });
   });
 
   // Listen for chat message
-  socket.on('chatMessage', async (msg) => {
+  socket.on("chatMessage", async (msg) => {
     const user = getCurrentUser(socket.id);
     const message = formatMessage(user.username, msg);
 
@@ -77,36 +74,27 @@ io.on('connection', (socket) => {
     });
 
     // Emit the message to the room
-    io.to(user.room).emit('message', message);
+    io.to(user.room).emit("message", message);
   });
 
   // Runs when client disconnects
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     const user = userLeave(socket.id);
 
     if (user) {
       io.to(user.room).emit(
-        'message',
+        "message",
         formatMessage(botName, `${user.username} has left the chat`)
       );
 
       // Send users and room info
-      io.to(user.room).emit('roomUsers', {
+      io.to(user.room).emit("roomUsers", {
         room: user.room,
         users: getRoomUsers(user.room),
       });
     }
   });
 });
-
-// app.get('/api/products', (req, res) => {
-//     const data = [
-//         { name: "affan", price: "book me in just 150₹", photo: "/assets/affan.png" },
-//         { name: "faizan", price: "book me in just 250₹ + GST + CGST", photo: "/assets/f.png" },
-//         // { name: "farhan", price: "book me in just 999₹", photo: "/assets/farh.png" },
-//     ];
-//     res.json(data);
-// });
 
 const PORT = process.env.PORT || 5000;
 
